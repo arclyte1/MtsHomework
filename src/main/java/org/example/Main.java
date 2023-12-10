311e6fd44241f878dbad0fe270c3336e973e5438
arclyte1 <arclyte18@gmail.com>
2023-12-10 21:14:58 +0300

    Moved printFinalCost static method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +9,19 @@ public class Main {
         Purchase purchase2 = new Purchase(100, 150, 42.575);
         Purchase purchase3 = new Purchase(100, 150, 59.1);
 
-        Purchase.printFinalCost(purchase1);
-        Purchase.printFinalCost(purchase2);
-        Purchase.printFinalCost(purchase3);
+        printFinalCost(purchase1);
+        printFinalCost(purchase2);
+        printFinalCost(purchase3);
+    }
+
+    public static void printFinalCost(Purchase purchase) {
+        double finalCostWithoutDiscount = purchase.productCost * purchase.productCount;
+        double finalCostWithDiscount = finalCostWithoutDiscount * (1 - purchase.discount / 100);
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        numberFormat.setMaximumFractionDigits(2);
+        numberFormat.setGroupingUsed(false);
+        System.out.println("Final cost without discount = " + numberFormat.format(finalCostWithoutDiscount));
+        System.out.println("Final cost with discount = " + numberFormat.format(finalCostWithDiscount));
     }
 }
--- src/main/java/org/example/Purchase.java
+++ src/main/java/org/example/Purchase.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.text.NumberFormat;
 
 /**
  * Purchase model.
@@ -27,16 +26,5 @@ public class Purchase {
         this.productCount = productCount;
         this.productCost = productCost;
         this.discount = discount;
-    }
-
-    public static void printFinalCost(Purchase purchase) {
-        double finalCostWithoutDiscount = purchase.productCost * purchase.productCount;
-        double finalCostWithDiscount = finalCostWithoutDiscount * (1 - purchase.discount / 100);
-
-        NumberFormat numberFormat = NumberFormat.getNumberInstance();
-        numberFormat.setMaximumFractionDigits(2);
-        numberFormat.setGroupingUsed(false);
-        System.out.println("Final cost without discount = " + numberFormat.format(finalCostWithoutDiscount));
-        System.out.println("Final cost with discount = " + numberFormat.format(finalCostWithDiscount));
     }
 }
